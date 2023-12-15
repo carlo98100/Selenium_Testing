3ad56834dd38d7d37ef22f0e8cdcd6004f65ed7d
cago-rebl <carlo.goretti@helo.se>
2023-12-15 11:43:56 +0100

    Fixed a bug with my TestRunner.java

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/registerSupporter.feature",
-        glue = "stepDefinitions.registerSupporterSteps.java"
+        glue = "stepDefinitions"
 )
+
 public class TestRunner {
 }
