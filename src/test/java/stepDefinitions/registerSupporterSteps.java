6ab74fc166c708e1657137bf665c16f15e5782e7
cago-rebl <carlo.goretti@helo.se>
2023-12-15 13:42:52 +0100

    Added a dependency

--- src/test/java/stepDefinitions/registerSupporterSteps.java
+++ src/test/java/stepDefinitions/registerSupporterSteps.java
@@ -5,6 +5,8 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.openqa.selenium.By;
@@ -19,76 +21,77 @@ import static java.lang.Thread.*;
 
 public class registerSupporterSteps {
 
-    WebDriver driver;
+WebDriver driver = null;
 
-    registerSupporterPage page;
+registerSupporterPage page;
 
-    @Given("I am on the registration page")
-    public void iAmOnTheRegistrationPage() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        driver = new ChromeDriver();
-        page = new registerSupporterPage(driver);
+@Given("I am on the registration page")
+public void iAmOnTheRegistrationPage() {
+WebDriverManager.chromedriver().config();
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+options.addArguments("--headless");
+driver = new ChromeDriver(options);
+page = new registerSupporterPage(driver);
 
-        driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
+driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
 
-    }
+}
 
-    @When("I provide my date of birth {string} and my firstname {string} and my lastname {string} and my email {string} and my password {string}")
-    public void iProvideMyDateOfBirthAndMyFirstnameAndMyLastnameAndMyEmailAndMyPassword(String dateOfBirth, String firstname, String lastname, String email, String password) {
-        registerSupporterPage page = new registerSupporterPage(driver);
-        page.registerValidSupporter(dateOfBirth, firstname, lastname, email, password);
+@When("I provide my date of birth {string} and my firstname {string} and my lastname {string} and my email {string} and my password {string}")
+public void iProvideMyDateOfBirthAndMyFirstnameAndMyLastnameAndMyEmailAndMyPassword(String dateOfBirth, String firstname, String lastname, String email, String password) {
+registerSupporterPage page = new registerSupporterPage(driver);
+page.registerValidSupporter(dateOfBirth, firstname, lastname, email, password);
 
-    }
+}
 
-    @And("User clicks on the Confirm and join button")
-    public void userClicksOnTheConfirmAndJoinButton() throws InterruptedException {
-        page.ClickOnConfirmAndJoinButton();
-    }
+@And("User clicks on the Confirm and join button")
+public void userClicksOnTheConfirmAndJoinButton() throws InterruptedException {
+page.ClickOnConfirmAndJoinButton();
+}
 
-    @Then("I should be on the confirmation page")
-    public void iShouldBeOnTheConfirmationPage() {
-        boolean isLockerButtonDisplayed = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[2]/a")).isDisplayed();
+@Then("I should be on the confirmation page")
+public void iShouldBeOnTheConfirmationPage() {
+boolean isLockerButtonDisplayed = driver.findElement(By.xpath("/html/body/div/div[2]/div/div/div[2]/a")).isDisplayed();
 
-        Assert.assertTrue(isLockerButtonDisplayed);
-        driver.quit();
-    }
+Assert.assertTrue(isLockerButtonDisplayed);
+driver.quit();
+}
 
-    @When("I provide my date of birth {string} and my firstname {string} and my email {string} and my password {string} but not the lastname")
-    public void iProvideMyDateOfBirthAndMyFirstnameAndMyEmailAndMyPasswordButNotTheLastname(String dateOfBirth, String firstname, String email, String password) {
-        page.registerSupporterWithoutLastname(dateOfBirth, firstname, email, password);
-    }
+@When("I provide my date of birth {string} and my firstname {string} and my email {string} and my password {string} but not the lastname")
+public void iProvideMyDateOfBirthAndMyFirstnameAndMyEmailAndMyPasswordButNotTheLastname(String dateOfBirth, String firstname, String email, String password) {
+page.registerSupporterWithoutLastname(dateOfBirth, firstname, email, password);
+}
 
-    @Then("User should get a error message telling that the lastname is required")
-    public void userShouldGetAErrorMessageTellingThatTheLastnameIsRequired() {
-        Assert.assertTrue(page.IsLastNameIsMissingErrorMessageDisplayed());
-        Assert.assertEquals("Last Name is required", page.LastNameIsMissingErrorMessage());
-        driver.quit();
-    }
+@Then("User should get a error message telling that the lastname is required")
+public void userShouldGetAErrorMessageTellingThatTheLastnameIsRequired() {
+Assert.assertTrue(page.IsLastNameIsMissingErrorMessageDisplayed());
+Assert.assertEquals("Last Name is required", page.LastNameIsMissingErrorMessage());
+driver.quit();
+}
 
-    @When("I provide my date of birth {string} and my firstname {string} and my lastname {string} and my email {string} and my password {string} and also confirms the password by providing the password {string} again")
-    public void iProvideMyDateOfBirthAndMyFirstnameAndMyLastnameAndMyEmailAndMyPasswordAndAlsoConfirmsThePasswordByProvidingThePasswordAgain(String dateOfBirth, String firstname, String lastname, String email, String password, String confirmPassword) {
-        page.registerSupporterWithUnmatchedPasswords(dateOfBirth, firstname, lastname, email, password, confirmPassword);
-    }
+@When("I provide my date of birth {string} and my firstname {string} and my lastname {string} and my email {string} and my password {string} and also confirms the password by providing the password {string} again")
+public void iProvideMyDateOfBirthAndMyFirstnameAndMyLastnameAndMyEmailAndMyPasswordAndAlsoConfirmsThePasswordByProvidingThePasswordAgain(String dateOfBirth, String firstname, String lastname, String email, String password, String confirmPassword) {
+page.registerSupporterWithUnmatchedPasswords(dateOfBirth, firstname, lastname, email, password, confirmPassword);
+}
 
-    @Then("User should get a error message telling that the passwords doesn't match")
-    public void userShouldGetAErrorMessageTellingThatThePasswordsDoesntMatch() {
-        Assert.assertTrue(page.IsUnmatchedPasswordsErrorMessageDisplayed());
-        Assert.assertEquals("Password did not match", page.UnmatchedPasswordsErrorMessage());
-        driver.quit();
-    }
+@Then("User should get a error message telling that the passwords doesn't match")
+public void userShouldGetAErrorMessageTellingThatThePasswordsDoesntMatch() {
+Assert.assertTrue(page.IsUnmatchedPasswordsErrorMessageDisplayed());
+Assert.assertEquals("Password did not match", page.UnmatchedPasswordsErrorMessage());
+driver.quit();
+}
 
-    @When("I provide my date of birth {string} and my firstname {string} and my lastname {string} and my email {string} and my password {string} but not the but don't accept the Terms & conditions")
-    public void iProvideMyDateOfBirthAndMyFirstnameAndMyLastnameAndMyEmailAndMyPasswordButNotTheButDontAcceptTheTermsConditions(String dateOfBirth, String firstname, String lastname, String email, String password) {
-        page.registerSupporterWithoutAcceptTermsAndConditions(dateOfBirth, firstname, lastname, email, password);
-    }
+@When("I provide my date of birth {string} and my firstname {string} and my lastname {string} and my email {string} and my password {string} but not the but don't accept the Terms & conditions")
+public void iProvideMyDateOfBirthAndMyFirstnameAndMyLastnameAndMyEmailAndMyPasswordButNotTheButDontAcceptTheTermsConditions(String dateOfBirth, String firstname, String lastname, String email, String password) {
+page.registerSupporterWithoutAcceptTermsAndConditions(dateOfBirth, firstname, lastname, email, password);
+}
 
-    @Then("User should get a error message telling that the user must accept the terms & conditions")
-    public void userShouldGetAErrorMessageTellingThatTheUserMustAcceptTheTermsConditions() {
-        Assert.assertTrue(page.IsTermsAndConditionsErrorMessageDisplayed());
-        Assert.assertEquals("You must confirm that you have read and accepted our Terms and Conditions", page.TermsAndConditionsErrorMessage());
-        driver.quit();
-    }
+@Then("User should get a error message telling that the user must accept the terms & conditions")
+public void userShouldGetAErrorMessageTellingThatTheUserMustAcceptTheTermsConditions() {
+Assert.assertTrue(page.IsTermsAndConditionsErrorMessageDisplayed());
+Assert.assertEquals("You must confirm that you have read and accepted our Terms and Conditions", page.TermsAndConditionsErrorMessage());
+driver.quit();
+}
 }
